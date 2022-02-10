55a71ba34a860edd13bdad16daceefe0c832e392
Tenzin Jorden <tenden96@gmail.com>
2022-02-10 12:34:06 -0800

    commit tree d368d171f4b6753de079a89a010bd74ae3c4c63f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,11 @@
-# Your code here!
+# def greet(name = " ")
+#     puts "Hello, #{name}!"
+# end
+
+# greet "Naureen"
+
+def test(a1 = "Ruby", a2 = "Perl")
+    puts "The programming language is #{a1}"
+    puts "The programming language is #{a2}"
+ end
+ test "C", "C++"
